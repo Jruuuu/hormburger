04a3cd06b636306db86b692782e65467e705d72a
Jruuuu <71367882+Jruuuu@users.noreply.github.com>
2021-01-08 11:49:30 -0800

    made file structure, added server.js, added schema and seed for db file

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES('IN-N-OUT DOUBLE DOUBLE BURGER');
+INSERT INTO burgers (burger_name) VALUES('WHOPPER');
+INSERT INTO burgers (burger_name) VALUES('BIGMAC');
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES('IN-N-OUT DOUBLE DOUBLE BURGER',true);
